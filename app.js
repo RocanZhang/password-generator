2bd95edd230dbdba1cf859a101d9bcf412d4e099
RocanZhang <rocan87447@gmail.com>
2024-08-30 23:30:12 +0800

    一般檢查

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-
-// app.get("/", (req, res) => {
-//   res.send(`測試頁面`);
-// });
-
-// app.listen(port, () => {
-//   console.log(`1st express server on http://localhost:${port}`);
-// });
-
-
 const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
-const password_generator = require('./main')
+const password_generator = require("./main");
 
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
@@ -23,7 +10,7 @@ app.set("views", "./views");
 app.use(express.static("public"));
 // app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 
